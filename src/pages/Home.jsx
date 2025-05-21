a5ef9c3a20b1ec547bb24b1498ea96e92410a555
Moiwa <samuelmoiwa@gmail.com>
2025-05-21 01:57:30 +0000

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,19 +10,36 @@ const Home = () => {
   return (
     <div className='w-full flex justify-center items-center'>
       <div className='w-screen h-full max-w-[130rem] flex justify-center items-center flex-col'>
-        <Banner/>
 
-        <div className='bg-gradient-to-b from-teal-100 via-white to-blue-100 w-full pt-14 flex flex-col justify-center
-          items-center justify-self-center'>
-          <HomeIntro />
-          <AboutUs/>
-          <CallToAction/>
+        <div id="home" className="w-full">
+          <Banner />
         </div>
 
-        <CarbonDevelopment/>
-        <MembersCarousel />
-      </div>
+        <div className='bg-gradient-to-b from-teal-100 via-white to-blue-100 w-full pt-14 flex flex-col justify-center items-center'>
+          <div id="home-info">
+            <HomeIntro />
+          </div>
 
+
+          <div id="about-us">
+            <AboutUs />
+          </div>
+
+          <CallToAction />
+        </div>
+
+        <div id="what-is-carbon-development" className="w-full">
+          <CarbonDevelopment />
+        </div>
+
+        <div id="our-members" className="w-full">
+          <MembersCarousel />
+        </div>
+
+        <div id="contact-us" className="w-full">
+          {/* You can create a ContactUs component and add it here */}
+        </div>
+      </div>
     </div>
   )
 }
